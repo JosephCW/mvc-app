16823b14e270eb2b80ea357f7c46c8761945a9bd
Joseph Watts <josephcw@live.com>
2019-11-05 09:51:52 -0600

    Add Teacher model #2

--- models/instructors.js
+++ models/instructors.js
@@ -1,75 +1,45 @@
 /**
-*  Developer model
-*  Describes the characteristics of each attribute in a developer resource.
+*  Instructor Model
+*  Describes the characteristics of each attribute in an instructor resource.
 *
-* @author Denise Case <denisecase@gmail.com>
+* @author Joseph Watts <s519653@nwmissouri.edu>
 * @requires mongoose
 *
 */
 const mongoose = require('mongoose')
 
-const DeveloperSchema = new mongoose.Schema({
+const InstructorSchema = new mongoose.Schema({
 
   _id: {
     type: Number,
-    required: true
+    unique: true
   },
   email: {
     type: String,
     minlength: 5,
     maxlength: 100,
-    required: true,
     unique: true
   },
   given: {
     type: String,
     minlength: 3,
     maxlength: 100,
-    required: false,
     default: 'Given name'
   },
   family: {
     type: String,
     minlength: 3,
     maxlength: 100,
-    required: false,
     default: 'Family name'
   },
-  city: {
-    type: String,
-    minlength: 3,
-    maxlength: 100,
+  salary: {
+    type: Number,
     required: true,
-    default: 'Maryville'
+    default: 0
   },
-  state: {
+  github: {
     type: String,
-    minlength: 2,
-    maxlength: 100,
-    required: true,
-    default: 'MO'
-  },
-  zip: {
-    type: String,
-    minlength: 5,
-    maxlength: 12,
-    required: true,
-    default: '64468'
-  },
-  country: {
-    type: String,
-    minlength: 5,
-    maxlength: 100,
-    required: true,
-    default: 'USA'
-  },
-  url: {
-    type: String,
-    minlength: 4,
-    maxlength: 100,
-    required: true,
-    default: 'http://yourwebsite.com'
+    default: "https://www.github.com/"
   }
-
 })
-module.exports = mongoose.model('Developer', DeveloperSchema)
+module.exports = mongoose.model('Instructor', InstructorSchema)
