05ee01b4531ea452aec70390c0b634609576e44a
Joseph Watts <josephcw@live.com>
2019-11-07 11:07:47 -0600

    Update comments in seeder.js

--- utils/seeder.js
+++ utils/seeder.js
@@ -1,46 +1,40 @@
-const Datastore = require('nedb') // set up a temporary (in memory) database
-//const developerData = require('../data/developers.json') // read in data file
+// set up a temporary (in memory) database
+const Datastore = require('nedb') 
+
+// Read in data that will be seeded later
 const instructorData = require('../data/instructors.json')
 const studentData = require('../data/students.json')
 const sectionData = require('../data/sections.json')
 const courseData = require('../data/courses.json')
 
-
 // inject Express app to configure it - EVERYTHING in through argument list
-
 module.exports = (app) => {
   console.log('START data seeder.')
   const db = {} // empty object to hold all collections
-
-  //db.developers = new Datastore() // new object property
-  //db.developers.loadDatabase() // call the loadDatabase method
+  // Create datastores and load empty database into db.x
   db.instructors = new Datastore()
   db.instructors.loadDatabase()
-
   db.students = new Datastore()
   db.students.loadDatabase()
-
   db.sections = new Datastore()
   db.sections.loadDatabase()
- 
   db.courses = new Datastore()
   db.course.loadDatabase()
-  // insert the sample data into our datastore
-  //db.developers.insert(developerData)
+
+  // Insert the sample data into our datastore
   db.instructors.insert(instructorData)
   db.students.insert(studentData)
   db.sections.insert(sectionData)
   db.courses.insert(courseData)
 
-  // initialize app.locals (these objects are available to the controllers)
-  //app.locals.developers = db.developers.find(developerData)
-  //console.log(`${app.locals.developers.query.length} developers seeded`)
+  // Initialize app.locals (these objects are available to the controllers)
   app.locals.instructors = db.instructors.find(instructorData)
   app.locals.students = db.students.find(studentData)
   app.locals.sections = db.sections.find(sectionData)
   app.locals.courses = db.courses.find(courseData)
   
-  console.log(`${app.locals.students.query.length} studentss seeded`)
+  // Update log to show quantity of each item seeded
+  console.log(`${app.locals.students.query.length} students seeded`)
   console.log(`${app.locals.instructors.query.length} instructors seeded`)
   console.log(`${app.locals.sections.query.length} sections seeded`)
   console.log(`${app.locals.courses.query.length} courses seeded`)
